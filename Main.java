52491acf0e15ee9c0eb0aa654d41255192707d8d
Thanathorn-TAconsel <s6301012610043@email.kmutnb.ac.th>
2021-08-19 20:00:39 -0700

    Test JFrame

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-class Main {
-    Main() {
+import javax.swing.JFrame;
 
+class Main {
+    JFrame f1 = new JFrame();
+    Main() {
+        f1.setBounds(100,100,100,100);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setVisible(true);
     }
     public static void main(String[] args) {
-            System.out.println("HellOWorld");    
+        new Main();
     }
 }
