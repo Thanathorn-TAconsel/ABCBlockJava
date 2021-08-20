86d225a430b936ceaa7032efbbc611df2b09714b
Thanathorn-TAconsel <s6301012610043@email.kmutnb.ac.th>
2021-08-19 20:08:07 -0700

    Test Canvas in Java

---
+++ Canvas.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Canvas extends JComponent {
+    public void paint(Graphics g){  
+        g.drawLine(30, 20, 80, 90);
+    }
+}
--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ class Main {
     Main() {
         window.setBounds(100,100,300,300);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
-        
+        window.add(new Canvas());
+        window.setVisible(true);   
     }
     public static void main(String[] args) {
         new Main();
     }
 }
+
