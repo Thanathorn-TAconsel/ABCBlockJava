812691b797f1db6cb4a99da2f4dd6fb79c274e80
ThannathornSomton <s6201012610010@email.kmutnb.ac.th>
2021-08-20 10:29:45 +0700

    draw game table test

--- Canvas.java
+++ Canvas.java
@@ -3,6 +3,11 @@ import javax.swing.*;
 
 public class Canvas extends JComponent {
     public void paint(Graphics g){  
-        g.drawLine(30, 20, 80, 90);
+        for(int y=0; y<3; y++) {
+            for(int x=0; x<4; x++ ) {
+                g.drawRect(x*100, y*100, 100, 100);
+
+            }
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ import javax.swing.JFrame;
 class Main {
     JFrame window = new JFrame("ABCBlocks");
     Main() {
-        window.setBounds(100,100,300,300);
+        window.setBounds(100,100,401,301);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.add(new Canvas());
+        window.setUndecorated(true);
         window.setVisible(true);   
     }
     public static void main(String[] args) {
